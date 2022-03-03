2ff0ce62c636e1c1fe2233fe6236879caf382c84
dliden-bitdotio <dliden@bit.io>
2022-03-03 15:17:34 -0800

    implement query and import functionality
    the import method now has options to append, truncate, or replace
    tables (default append to avoid data loss).

--- src/bit.jl
+++ src/bit.jl
@@ -28,6 +28,23 @@ function install_key!(bitio_pg_string::AbstractString)
     @info("bit.io pg_string and password saved to LocalPreferences.toml. Make sure to add this file to your .gitignore!")
 end
 
+"""Executes a query on bit.io."""
+function query!(query; pg_string=missing)
+    if ismissing(pg_string)
+        pg_string = @load_preference("bitio_pg_string", missing)
+        if ismissing(pg_string)
+            throw(ErrorException("Please include a pg_string argument or install your pg_string with the install_key! method"))
+        end
+    end
+
+    query = sql`$query`
+    result = LibPQ.Connection(pg_string) do conn
+        execute(conn, query.args[1])
+    end
+    return result
+end
+
+
 """Downloads a full table given the pg_string, username,
    schema name, and table name"""
 function download_table(username, schema, tablename; pg_string=missing)
@@ -45,7 +62,8 @@ end
 
 """Given a DataFrame, imports the DataFrame to the Database
    table/schema defined by the username, tablename, schema fields"""
-function import!(df, username, schema, tablename; bitio_key=missing)
+function import!(df, username, schema, tablename; bitio_key=missing,
+                 create_table_if_not_exists=true, if_exists="append")
     if ismissing(bitio_key)
         bitio_key = @load_preference("bitio_key", missing)
         if ismissing(bitio_key)
@@ -53,8 +71,16 @@ function import!(df, username, schema, tablename; bitio_key=missing)
         end
     end
 
+    if if_exists=="truncate"
+        query!("""TRUNCATE "$username/$schema"."$tablename";""")
+    elseif if_exists=="replace"
+        query!("""DROP TABLE "$username/$schema"."$tablename";""")
+    elseif if_exists != "append"
+        throw(ErrorException("if_exists must be one of 'append', 'truncate', or 'replace'"))
+    end
+
     url = "https://api.bit.io/api/v1beta/import/json/"
-    payload = Dict("create_table_if_not_exists" => true,
+    payload = Dict("create_table_if_not_exists" => create_table_if_not_exists,
 		"table_name" => tablename,
 		"repo_name" => schema,
 		"data" => arraytable(df))
